51e1f14c753a7d0f6f2f9378b1589a9bdf638975
Jing Ling <lingjing0921@live.com>
2024-01-02 17:20:33 +0800

    Change formatter#

---
+++ src/Ling.Tools.Core/Format/JsonFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace Ling.Tools.Format;
+
+/// <summary>
+/// Represents a formatter that can be used to beautify and minify JSON.
+/// </summary>
+public static class JsonFormatter
+{
+    private static readonly JsonSerializerOptions _formatOptions;
+    private static readonly JsonSerializerOptions _minifyOptions;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="JsonFormatter"/> class.
+    /// </summary>
+    static JsonFormatter()
+    {
+        _formatOptions = new() { WriteIndented = true };
+        _minifyOptions = new() { WriteIndented = false };
+    }
+
+    /// <inheritdoc />
+    public static string Beautify(string text)
+    {
+        var doc = JsonDocument.Parse(text);
+        return JsonSerializer.Serialize(doc.RootElement, _formatOptions);
+    }
+
+    /// <inheritdoc />
+    public static string Minify(string text)
+    {
+        var doc = JsonDocument.Parse(text);
+        return JsonSerializer.Serialize(doc.RootElement, _minifyOptions);
+    }
+}
